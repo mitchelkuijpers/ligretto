4bcb59863c1ef3b1e82c77ec4219a57354c1d000
Mitchel Kuijpers <m.kuijpers@avisi.nl>
2014-01-04 14:31:47 +0100

    simple console.log stuff with sockets

--- app.js
+++ app.js
@@ -38,3 +38,10 @@ var io = require('socket.io').listen(server);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
